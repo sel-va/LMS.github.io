21d47f9b42b324c860ccdcc558c5b2562dcee16b
er.ks.selva@gmail.com <er.ks.selva@gmail.com>
2023-01-17 19:35:08 +0530

    for level four changes

--- GIT_Practice/GIT_level_four_rebase.bash
+++ GIT_Practice/GIT_level_four_rebase.bash
@@ -1,6 +1,7 @@
 #GIT TASK THREE - Make 2 branch, Make conflict merge, REBASE the central repositry
 
 #SIMPLE REBASE
+=================
 
 git log --oneline
 
@@ -13,41 +14,43 @@ git branch branch2
 # switch to branch1
 git checkout branch1
 
-# make some changes to a file
-vi file.txt
+		# make some changes to a file
+		vi file.txt
 
-# commit the changes
-git add file.txt
-git status -s
-git commit -am "1st commit from branch1"
+		# commit the changes
+		git add file.txt
+		git status -s
+		git commit -am "1st commit from branch1"
 
-git checkout main
-git merge branch1
+		git checkout main
+		git merge branch1
 
 # switch to branch2
 git checkout branch2
 
 git checkout main
-git log --oneline
+		git log --oneline
 
-#use last commit_ID to view last commit details (by whome)
-git show f804ecd
+		#use last commit_ID to view last commit details (by whome)
+		git show f804ecd
 
 git checkout branch2
-git rebase main
+		git rebase main
 
-# make some different changes to the same file
-vi file.txt
+		# make some different changes to the same file
+		vi file.txt
 
-# commit the changes
-git add file.txt
-git commit -am "2nd time 1st commit from branch2"
+		# commit the changes
+		git add file.txt
+		git commit -am "2nd time 1st commit from branch2"
 
 git checkout main
-git merge branch1
+		git merge branch1
 
 #-------------------------------------------------------------
 # other way to rebase from commited branch2
+============================================
+
 # switch back to branch1
 git checkout branch2
 git branch
@@ -55,85 +58,139 @@ git branch
 # merge branch1 into branch2
 git merge branch1
 
+-------------------------------------------------------------------------------------------------------------
+#REBASE THEN FIX CONFLICTS
+===========================
+CREATE USER_1_2 > USER1 ADD TEXT > STAGE ALL > UNSTAGE > 
+
+$ git branch user1
+$ git branch user2
+$ git branch user3
+
+$ git checkout user1
+
+	$ vi tempfile.txt
+				$ git add .
+
+				$ git status -s
+				A  "GIT_Practice/List of git task questions with answer hint.bash"
+				A  HTML/HTML_with_CSS_basic.html
+				A  HTML/IT_JOBS_HTML_BASIC.html
+				A  tempfile.txt
+
+				# to unstage particular file
+				$ git reset tempfile.txt
+
+				$ git status -s
+				A  "GIT_Practice/List of git task questions with answer hint.bash"
+				A  HTML/HTML_with_CSS_basic.html
+				A  HTML/IT_JOBS_HTML_BASIC.html
+				?? tempfile.txt
+
+				# to unstage all files
+				$ git reset
+
+	$ git status -s
+	$ git add tempfile.txt
+	$ git commit -m "1st commit"
+
+$ git checkout user2
+	$ vi tempfile.txt
+
+	$ git status -s
+	$ git add tempfile.txt
+	$ git commit -m "2nd commit"
+
+$ git checkout main
+	$ git merge user1
+
+	$ git log -s
+
+$ git checkout user2
+
+	USER@Selva MINGW64 /d/lms/LMS.github.io (user2)
+	$ git branch
+	  main
+	  user1
+	* user2
+
+	$ ls -s
+	$ git rebase main
+	Auto-merging tempfile.txt
+	CONFLICT (add/add): Merge conflict in tempfile.txt
+	error: could not apply 07d625d... 2nd commit
+	hint: Resolve all conflicts manually, mark them as resolved with
+	hint: "git add/rm <conflicted_files>", then run "git rebase --continue".
+	hint: You can instead skip this commit: run "git rebase --skip".
+	hint: To abort and get back to the state before "git rebase", run "git rebase --abort".
+	Could not apply 07d625d... 2nd commit
+
+	$ vi tempfile.txt
+
+	$ git status -s
+	AA tempfile.txt
+	?? HTML/
+
+	$ git add tempfile.txt
+	$ git status -s
+	$ git rebase --continue
+	$ cat tempfile.txt
+
+#---------------------------------------------------------------------------------------------------------
+#STASH AND THEN REBASE
+========================
+
+$ git checkout user3
+$ vi tempfile.txt
+$ git add tempfile.txt
+
+$ cat tempfile.txt
+$ git status -s
+ M tempfile.txt
+?? HTML/
+
+$ git log --oneline
+
+$ git rebase main
+error: cannot rebase: You have unstaged changes.
+error: Please commit or stash them.
+
+$ git reset tempfile.txt
+
+USER@Selva MINGW64 /d/lms/LMS.github.io (user3)
+$ git rebase main
+error: cannot rebase: You have unstaged changes.
+error: Please commit or stash them.
+
+$ git stash
+$ cat tempfile.txt
+
+$ git stash list
+stash@{0}: WIP on user3: 1161c46 1st commit
+
+USER@Selva MINGW64 /d/lms/LMS.github.io (user3)
+$ git stash apply
+Auto-merging tempfile.txt
+CONFLICT (content): Merge conflict in tempfile.txt
+
+$ vi tempfile.txt
+
+USER@Selva MINGW64 /d/lms/LMS.github.io (user3)
+$ git status -s
+UU tempfile.txt
+?? HTML/
+
+$ git add tempfile.txt
+$ git commit -m "third scnrio"
+$ git status -s
+
+$ git checkout main
+		Switched to branch 'main'
+
+		$ git merge user3
+		$ git push
+
 ---------------------------------------------------------
 #other topic
 # update the master branch with the latest changes from the remote repository
 git pull origin main
-
--------------------------------------------------------------------------------------------------------------
-
-$ git branch user1
-$ git branch user2
-
-$ git checkout user1
-
-$ vi tempfile.txt
-$ git add .
-
-$ git status -s
-A  "GIT_Practice/List of git task questions with answer hint.bash"
-A  HTML/HTML_with_CSS_basic.html
-A  HTML/IT_JOBS_HTML_BASIC.html
-A  tempfile.txt
-
-# to unstage particular file
-$ git reset tempfile.txt
-
-$ git status -s
-A  "GIT_Practice/List of git task questions with answer hint.bash"
-A  HTML/HTML_with_CSS_basic.html
-A  HTML/IT_JOBS_HTML_BASIC.html
-?? tempfile.txt
-
-# to unstage all files
-$ git reset
-
-$ git status -s
-$ git add tempfile.txt
-$ git commit -m "1st commit"
-
-$ git checkout user2
-$ vi tempfile.txt
-
-$ git status -s
-$ git add tempfile.txt
-$ git commit -m "2nd commit"
-
-$ git checkout main
-
-$ git merge user1
-
-$ git log -s
-
-$ git checkout user2
-
-USER@Selva MINGW64 /d/lms/LMS.github.io (user2)
-$ git branch
-  main
-  user1
-* user2
-
-$ ls -s
-
-$ git rebase main
-Auto-merging tempfile.txt
-CONFLICT (add/add): Merge conflict in tempfile.txt
-error: could not apply 07d625d... 2nd commit
-hint: Resolve all conflicts manually, mark them as resolved with
-hint: "git add/rm <conflicted_files>", then run "git rebase --continue".
-hint: You can instead skip this commit: run "git rebase --skip".
-hint: To abort and get back to the state before "git rebase", run "git rebase --abort".
-Could not apply 07d625d... 2nd commit
-
-$ vi tempfile.txt
-
-$ git status -s
-AA tempfile.txt
-?? HTML/
-
-$ git add tempfile.txt
-$ git status -s
-$ git rebase --continue
-$ cat tempfile.txt
-
-#------------------------------------------------------------------------------------------------
--- GIT_Practice/GIT_level_two.bash
+++ GIT_Practice/GIT_level_two.bash
@@ -1,3 +1,9 @@
+#other topic
+# update the master branch with the latest changes from the remote repository
+git pull origin main
+
+#----------------------------------------------------------------------------------------------------------
+
 #GIT TASK TWO - Make 2 branch, Make soft merge, Push it to a remote repository
   #Create a new branch in your repository and make changes to it.
   #Merge the changes from the new branch back into the master branch.
@@ -17,43 +23,42 @@ $ git branch
 * main
 
 $ git checkout git_practice
-$ git branch
-* git_practice
-  main
+		$ git branch
+		* git_practice
+		  main
 
-$ ls -lrt
-$ cd plsql
-$ pwd
-#/d/LMS/LMS.github.io/plsql
+		$ ls -lrt
+		$ cd plsql
+		$ pwd
+		#/d/LMS/LMS.github.io/plsql
 
-$ ls -lrt
-$ git status
-$ git add plslq_code_to_print_calender.sql
-$ git status -s
+		$ ls -lrt
+		$ git status
+		$ git add plslq_code_to_print_calender.sql
+		$ git status -s
 
-$ git commit -m "calender_plslq_code"
-$ git status -s
+		$ git commit -m "calender_plslq_code"
+		$ git status -s
 
 #Merge the changes from the new branch back into the master branch.
-
 $ git checkout main
-$ git branch
-$ git merge git_practice
+		$ git branch
+		$ git merge git_practice
 
-$ git branch
-$ ls -lrt
-  
-#Push your local repository to a remote repository on a Git hosting service (e.g. GitHub).
+		$ git branch
+		$ ls -lrt
+		  
+		#Push your local repository to a remote repository on a Git hosting service (e.g. GitHub).
 
-$ git status -s
-$ git push
-$ git branch
-$ git status -s
+		$ git status -s
+		$ git push
+		$ git branch
+		$ git status -s
 
-#Delete that new branch in your repository
+		#Delete that new branch in your repository
 
-$ git branch -d git_practice
-Deleted branch git_practice (was 0df4a91).
+		$ git branch -d git_practice
+		Deleted branch git_practice (was 0df4a91).
 
-$ git branch
-* main
+		$ git branch
+		* main
