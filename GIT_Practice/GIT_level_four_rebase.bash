0cc8db0393a2dd4925fe5e35bfc5fcb6143cec19
er.ks.selva@gmail.com <er.ks.selva@gmail.com>
2023-01-17 18:39:29 +0530

    third scnrio

--- GIT_Practice/GIT_level_four_rebase.bash
+++ GIT_Practice/GIT_level_four_rebase.bash
@@ -16,6 +16,7 @@ vi file.txt
 
 # commit the changes
 git add file.txt
+git status -s
 git commit -am "1st commit from branch1"
 
 # switch to branch2
@@ -29,9 +30,9 @@ git add file.txt
 git commit -am "2nd time 1st commit from branch2"
 
 #-------------------------------------------------------------
-git merge branch1
-
 git checkout main
+
+git merge branch1
 
 git log --oneline
 
@@ -59,4 +60,79 @@ git merge branch2
 #other topic
 # update the master branch with the latest changes from the remote repository
 git pull origin main
+
+-------------------------------------------------------------------------------------------------------------
+$ git branch user1
+$ git branch user2
+
+$ git checkout user1
+
+$ vi tempfile.txt
+$ git add .
+
+$ git status -s
+A  "GIT_Practice/List of git task questions with answer hint.bash"
+A  HTML/HTML_with_CSS_basic.html
+A  HTML/IT_JOBS_HTML_BASIC.html
+A  tempfile.txt
+
+# to unstage particular file
+$ git reset tempfile.txt
+
+$ git status -s
+A  "GIT_Practice/List of git task questions with answer hint.bash"
+A  HTML/HTML_with_CSS_basic.html
+A  HTML/IT_JOBS_HTML_BASIC.html
+?? tempfile.txt
+
+# to unstage all files
+$ git reset
+
+$ git status -s
+$ git add tempfile.txt
+$ git commit -m "1st commit"
+
+$ git checkout user2
+$ vi tempfile.txt
+
+$ git status -s
+$ git add tempfile.txt
+$ git commit -m "2nd commit"
+
+$ git checkout main
+
+$ git merge user1
+
+$ git log -s
+
+$ git checkout user2
+
+USER@Selva MINGW64 /d/lms/LMS.github.io (user2)
+$ git branch
+  main
+  user1
+* user2
+
+$ ls -s
+
+$ git rebase main
+Auto-merging tempfile.txt
+CONFLICT (add/add): Merge conflict in tempfile.txt
+error: could not apply 07d625d... 2nd commit
+hint: Resolve all conflicts manually, mark them as resolved with
+hint: "git add/rm <conflicted_files>", then run "git rebase --continue".
+hint: You can instead skip this commit: run "git rebase --skip".
+hint: To abort and get back to the state before "git rebase", run "git rebase --abort".
+Could not apply 07d625d... 2nd commit
+
+$ vi tempfile.txt
+
+$ git status -s
+AA tempfile.txt
+?? HTML/
+
+$ git add tempfile.txt
+$ git status -s
+$ git rebase --continue
+$ cat tempfile.txt
 
